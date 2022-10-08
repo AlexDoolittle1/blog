43444b51e1d928c980b0c9c5064422d9009985f9
Alex <alex.doolittle.2001@gmail.com>
2022-10-08 12:04:12 -0600

    home error fix 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import SignUpView, HomePageView
+from .views import SignUpView
 
 urlpattens = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("", HomePageView.as_view(), name="home")
+    path("signup/", SignUpView.as_view(), name="signup")
 ]
